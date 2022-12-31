33561ecbaf272676b128d4ce08be98ebe61dea4d
lowrollr <marshingjay@gmail.com>
2022-12-31 14:52:26 -0600

    belief net overhaul

--- net.py
+++ net.py
@@ -35,23 +35,57 @@ class ConvolutionalLayer(nn.Module):
 
 
 class BeliefNet(nn.Module):
-    def __init__(self, input_layers, output_layers, residual_layers=20) -> None:
+    def __init__(self, 
+                input_layers, main_layers=15, head_layers=5) -> None:
         super().__init__()
-        self.conv1 = ConvolutionalLayer(input_layers, 128)
-        layers = []
-        for _ in range(residual_layers):
+        
+        layers = [ConvolutionalLayer(input_layers, 128)]
+        for _ in range(main_layers):
             r = ResidualLayer(128, 128)
             layers.append(r)
-        self.residual_layers = nn.Sequential(*layers)
-        self.conv2 = ConvolutionalLayer(128, output_layers, k_size=1, padding=0)
-        self.passant_flatten = nn.Flatten()
-        self.passant_layer = nn.Linear(64, 8)
-        self.castle_flatten = nn.Flatten()
-        self.castle_layer = nn.Linear(64, 2)
+        layers.append(ConvolutionalLayer(128, 128))
+        self.main_vein = nn.Sequential(*layers)
+        
+        board_layers = []
+        pieces_layers = []
+        ep_layers = []
+        castle_layers = []
+        for _ in range(head_layers):
+            board_layers.append(ResidualLayer(128,128))
+            pieces_layers.append(ResidualLayer(128,128))
+            ep_layers.append(ResidualLayer(128,128))
+            castle_layers.append(ResidualLayer(128,128))
+
+        board_layers.append(ConvolutionalLayer(128,7, k_size=1, padding=0))
+        board_layers.append(nn.Sigmoid())
+        board_layers.append(nn.Softmax(dim=1))
+
+        pieces_layers.append(ConvolutionalLayer(128,5, k_size=1, padding=0))
+        pieces_layers.append(nn.Flatten(start_dim=2))
+        pieces_layers.append(nn.Linear(64,8))
+        pieces_layers.append(nn.Sigmoid())
+        pieces_layers.append(nn.Softmax(dim=1))
+        
+        ep_layers.append(ConvolutionalLayer(128,1, k_size=1, padding=0))
+        ep_layers.append(nn.Flatten(start_dim=1))
+        ep_layers.append(nn.Linear(64,9))
+        ep_layers.append(nn.Sigmoid())
+        ep_layers.append(nn.Softmax(dim=1))
+
+        castle_layers.append(ConvolutionalLayer(128,1, k_size=1, padding=0))
+        castle_layers.append(nn.Flatten(start_dim=1))
+        castle_layers.append(nn.Linear(64,2))
+        castle_layers.append(nn.Sigmoid())
+
+        self.board_layers = nn.Sequential(*board_layers)
+        self.pieces_layers = nn.Sequential(*pieces_layers)
+        self.ep_layers = nn.Sequential(*ep_layers)
+        self.castle_layers = nn.Sequential(*castle_layers)
+
         # self.mse_loss = nn.MSELoss()
         self.mse_loss = nn.MSELoss()
-        # self.WEIGHTS = torch.from_numpy(np.stack([np.full((8,8), 18), np.full((8,8),9), np.full((8,8),5), np.full((8,8),3), np.full((8,8),3), np.full((8,8),1)]))    
-        
+
+
     def weighted_mse_loss(self, input, target):
         return torch.mean(self.WEIGHTS * ((input - target) ** 2))
 
@@ -60,31 +94,27 @@ class BeliefNet(nn.Module):
         return next(self.parameters()).device
 
     def migrate_submodules(self):
-        self.conv1 = self.conv1.to(self.device)
-        self.conv2 = self.conv2.to(self.device)
-        new_residual_layers = []
-        for r in self.residual_layers:
-            new_residual_layers.append(r.to(self.device))
-        self.residual_layers = nn.Sequential(*new_residual_layers)
-        # self.WEIGHTS = self.WEIGHTS.to(self.device)
+        self.main_vein = self.main_vein.to(self.device)
+        self.board_layers = self.board_layers.to(self.device)
+        self.pieces_layers = self.pieces_layers.to(self.device)
+        self.ep_layers = self.ep_layers.to(self.device)
+        self.castle_layers = self.castle_layers.to(self.device)
+
 
     def forward(self, x):
-        x = self.conv1(x)
-        for r in self.residual_layers:
-            x = r(x)
-        x = self.conv2(x) 
-        x = torch.sigmoid(x)
+        x = self.main_vein(x)
 
-        probs, passant, castle = torch.split(x, (6, 1, 1), dim=1)
-        castle = torch.sigmoid(self.castle_layer(self.castle_flatten(castle)))
-        passant = torch.sigmoid(self.passant_layer(self.passant_flatten(passant)))
-        return probs, passant, castle
+        board = self.board_layers(x)
+        pieces = self.pieces_layers(x)
+        ep = self.ep_layers(x)
+        castle = self.castle_layers(x)
+
+        return board, pieces, ep, castle
+
 
     def loss_fn(self, inp, output, actual) -> torch.TensorType:
         # slice input to yield the same as expected output
-        inp = inp[:,14:20,:,:], inp[:,20,:,0], inp[:,21,0,3:5]
+        inp = inp[:,14:20,:,:], inp[:,20,:,0], inp[:,21,0,3:5], inp[:,4,0:5,:]
         
-
-
         return sum([self.mse_loss(a, b) for a,b in zip(output, actual)]) \
                - sum([self.mse_loss(a, b) for a,b in zip(inp, actual)])
